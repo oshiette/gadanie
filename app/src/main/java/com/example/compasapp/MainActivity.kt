4b398b641dd3ef10deeae4eedc85d6ec4ee63141
oshiette <nepovar.228@mail.ru>
2023-04-26 12:39:50 +0300

    with music

--- app/src/main/java/com/example/compasapp/MainActivity.kt
+++ app/src/main/java/com/example/compasapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.compasapp
 
 import android.content.Intent
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -10,6 +11,8 @@ import com.example.compasapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var button: Button
+    private lateinit var mediaPlayer: MediaPlayer
+
     lateinit var bind: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +23,25 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, SecondActivity::class.java)
             startActivity(intent)
         }
+            val playButton = findViewById<Button>(R.id.btnMusic)
+            playButton.setOnClickListener {
+// Если mediaPlayer уже проигрывает музыку, остановить ее и вернуться к началу
+                if (mediaPlayer.isPlaying) {
+                    mediaPlayer.pause()
+                    mediaPlayer.seekTo(0)
+                } else {
+                    // Иначе начать проигрывание музыки
+                    mediaPlayer.start()
+                }
+            }
+            // Загрузить музыкальный файл из папки res/raw
+            mediaPlayer = MediaPlayer.create(this, R.raw.music)
+
+        fun onDestroy() {
+            super.onDestroy()
+            mediaPlayer.release()
+        }
     }
-}
+    }
+
 
--- app/src/main/java/com/example/compasapp/SecondActivity.kt
+++ app/src/main/java/com/example/compasapp/SecondActivity.kt
@@ -1,20 +1,36 @@
 package com.example.compasapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.compasapp.databinding.ActivityMainBinding
 import com.example.compasapp.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
     lateinit var binding: ActivitySecondBinding
+    lateinit var but: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         binding = ActivitySecondBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        but = findViewById(R.id.imbt)
+        but.setOnClickListener {
+            intent
+            finish()
+        }
         binding.tv1.setOnClickListener {
             binding.tv1.text = getPreds()
+
+
         }
+
+        fun onBack() {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
     }
     private fun getPreds(): String{
         return resources.getStringArray(R.array.preds)[randomNum()]
@@ -23,4 +39,5 @@ class SecondActivity : AppCompatActivity() {
         val size = resources.getStringArray(R.array.preds).size - 1
         return (0..size).random()
     }
+
 }
